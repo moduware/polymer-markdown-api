e07169fc008b456778cfdf6cdea85d1ef116c545
Clint <clintp1@gmail.com>
2017-12-05 09:31:47 +1100

    setup to use current directory and take a filename to parse

--- polymer-markdown-api.js
+++ polymer-markdown-api.js
@@ -1,28 +1,35 @@
 const {Analyzer, FSUrlLoader} = require('polymer-analyzer');
+
+const myFile = process.argv[2];
+const baseUrl = process.cwd();
 var markdown = "";
 
 let analyzer = new Analyzer({
-  urlLoader: new FSUrlLoader('/Users/Clint/Projects/moduware/morph-button'),
+  urlLoader: new FSUrlLoader(baseUrl),
 });
-
 // This path is relative to the package root
-analyzer.analyze(['./morph-button.html']).then((analysis) => {
-  // Print the name of every property on paper-button, and where it was
-  // inherited from.
 
-  const [MorphButton, ] = analysis.getFeatures(
-      {kind: 'element', id: 'morph-button', externalPackages: true});
+if (process.argv[2]) {
+  analyzer.analyze([myFile]).then((analysis) => {
+    // Print the name of every property on paper-button, and where it was
+    // inherited from.
   
-  
-  
-  if (MorphButton) {
-    propertyFormatter(MorphButton);
-  } else {
-    console.log(`File don't define or import morph-button.`);
-  }
-
-  console.log(markdown);
-});
+    const [MorphButton, ] = analysis.getFeatures(
+        {kind: 'element', id: 'morph-button', externalPackages: true});
+    
+    
+    
+    if (MorphButton) {
+      propertyFormatter(MorphButton);
+    } else {
+      console.log(`File don't define or import morph-button.`);
+    }
+    console.log(markdown);
+    
+  });
+} else {
+  console.log('Please pass in a filename of your polymer element');
+}
 
 function propertyFormatter(element) {
   markdown += "## Properties\n";
@@ -30,4 +37,8 @@ function propertyFormatter(element) {
     markdown += `**${property.name}**: _${property.type}_ ${typeof (property.default) == 'undefined' ? '' : ' = \`\`' + property.default + '\`\`'}\n\n
       ${property.description}\n\n\n `;
   }
+}
+
+exports.printMsg = function() {
+  console.log('This is from polymer-markdown-api');
 }
