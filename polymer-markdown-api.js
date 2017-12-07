e7ddd7417c7c5db12f6bce22e5eb1df781250f7b
Clint <clintp1@gmail.com>
2017-12-07 14:13:28 +1100

    refactor code to take in arguments correctly

--- polymer-markdown-api.js
+++ polymer-markdown-api.js
@@ -1,55 +1,53 @@
 #! /usr/bin/env node
-
+const VERSION = '1.0.0';
 const {Analyzer, FSUrlLoader} = require('polymer-analyzer');
 const program = require('commander');
-
+const fs = require('fs');
 const baseUrl = process.cwd(); // gets the path of the current directory
 
-const myArgs = process.argv.slice(2); // strip 2 default args we don't need
-const myFile = myArgs[0]; // gets first argument pass in the command line
-const elementId = myFile.substr(0, myFile.lastIndexOf('.')) || myFile; // strip the filename of file extension
+var markdown = '';
 
-const analyzer = new Analyzer({
-  urlLoader: new FSUrlLoader(baseUrl),
-});
-
-var markdown = "";
+// This is the top part of our md file
+console.log(`Markdown Generator ${VERSION}\n`);
 
 program
-  .version('1.0.0')
+  .version(VERSION)
   .usage('[options] <file>')
   .option('-e, --element-name <name>', 'Element tag name if different from filename')
   .option('-o, --output-file <file>', 'Filename to save result')
   .parse(process.argv);
 
-
-console.log('--element-name', program.elementName);
-console.log('--output-file', program.outputFile);
-console.log('filename', program.args[0]);
-
-return;
-
-// This is the top part of our md file
-console.log(`# This is Markdown Generator\n`);
-console.log(`The baseURL is ${baseUrl}`);
-console.log(`and file pass in is <${myFile}>\n`);
-
-if (myFile) {
-  analyzer.analyze([myFile]).then((analysis) => {
-    const [MorphButton, ] = analysis.getFeatures(
-      { kind: 'element', id: elementId, externalPackages: true});
-    
-    if (MorphButton) {
-      propertyFormatter(MorphButton);
-    } else {
-      console.log(`File don't define or import morph-button.`);
-    }
-    console.log(markdown);
-    
-  });
-} else {
-  console.log('Please pass in a filename of your polymer element');
+if (program.args.length != 1) {
+  console.log('Single file to process must be specified');
+  return;
 }
+const targetFile = program.args[0];
+const targetFileBaseName = targetFile.replace(/^.*[\\\/]/, '');
+const targetFileDir = targetFile.replace(targetFileBaseName, '');
+const elementId = program.elementName || targetFileBaseName.substr(0, targetFileBaseName.lastIndexOf('.'));
+const outputName = program.outputFile || 'REFERENCE.md';
+
+console.log(`Processing <${targetFile}>\n`);
+
+const analyzer = new Analyzer({
+  urlLoader: new FSUrlLoader(targetFileDir),
+});
+
+analyzer.analyze([targetFileBaseName]).then((analysis) => {
+  const [ElementClass, ] = analysis.getFeatures(
+    { kind: 'element', id: elementId, externalPackages: true});
+  
+  if (!ElementClass) {
+    console.log(`File don't define or import ${elementId}.`);
+    return;
+  }
+
+  propertyFormatter(ElementClass);
+
+  fs.writeFileSync(outputName, markdown);
+  console.log(`Markdown saved to ${outputName}.`);
+
+});
 
 function propertyFormatter(element) {
   markdown += "## Properties\n\n";
