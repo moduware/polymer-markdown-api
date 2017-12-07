b22ddba6c7673a5b73bb3c07dec335e1358e1c6a
Clint <clintp1@gmail.com>
2017-12-07 13:38:47 +1100

    add commander npm package as dependency
    set up flags that we need to take in required arguments

--- polymer-markdown-api.js
+++ polymer-markdown-api.js
@@ -1,17 +1,33 @@
 #! /usr/bin/env node
 
 const {Analyzer, FSUrlLoader} = require('polymer-analyzer');
+const program = require('commander');
 
-const baseUrl = process.cwd();
-var markdown = "";
+const baseUrl = process.cwd(); // gets the path of the current directory
 
-var myArgs = process.argv.slice(2); // strip 2 default args we don't need
-var myFile = myArgs[0];
-const elementId = myFile.substr(0, myFile.lastIndexOf('.')) || myFile;
+const myArgs = process.argv.slice(2); // strip 2 default args we don't need
+const myFile = myArgs[0]; // gets first argument pass in the command line
+const elementId = myFile.substr(0, myFile.lastIndexOf('.')) || myFile; // strip the filename of file extension
 
-let analyzer = new Analyzer({
+const analyzer = new Analyzer({
   urlLoader: new FSUrlLoader(baseUrl),
 });
+
+var markdown = "";
+
+program
+  .version('1.0.0')
+  .usage('[options] <file>')
+  .option('-e, --element-name <name>', 'Element tag name if different from filename')
+  .option('-o, --output-file <file>', 'Filename to save result')
+  .parse(process.argv);
+
+
+console.log('--element-name', program.elementName);
+console.log('--output-file', program.outputFile);
+console.log('filename', program.args[0]);
+
+return;
 
 // This is the top part of our md file
 console.log(`# This is Markdown Generator\n`);
