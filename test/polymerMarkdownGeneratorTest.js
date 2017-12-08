f9a202366b02394e300a97518a0a9b6c2b54bb99
Clint <clintp1@gmail.com>
2017-12-08 17:34:42 +1100

    add test file and add example test

---
+++ test/polymerMarkdownGeneratorTest.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+var generator = require('../polymer-markdown-generator.js');
+
+describe('Polymer Markdown Generator', function() {
+  
+  context('methodFormatter', function() {
+    it('test for method formatter output', function() {
+      var arr = [];
+      
+      expect(arr.length).to.equal(0);
+    });
+
+    it('should have a methodFormatter Method', function() {
+      expect(typeof generator).to.be.equal('object');
+    });
+  });
+  
+  context('propertyFormatter', function() {
+    it('test for property formatter output');
+  });
+
+  context('createStringOfArguments', function(){
+    it('takes an array and returns comma seperated string of names');
+  });
+});
