01e982884c6e44ae013b5fc9ca769e1c0a7ceaab
Clint <clintp1@gmail.com>
2017-12-12 09:29:08 +1100

    created file method-formatter.js

---
+++ method-formatter.js
@@ -0,0 +1,12 @@
+function methodFormatter(element) {
+  let markdown = '';
+  markdown += '## Methods\n\n';
+
+  for (const [name, method] of element.methods) {
+    let argumentNames = createStringOfArgumentNames(method.params);
+    markdown += `**${method.name}(${argumentNames})**: _${method.return.type}_ \n\n${method.description}\n\n\n`;
+  }
+  return markdown;
+}
+
+module.exports = methodFormatter;
