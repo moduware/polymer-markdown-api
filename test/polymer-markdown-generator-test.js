3010b148e039145ecc1a4b8a06694146d775e661
Clint <clintp1@gmail.com>
2017-12-13 13:51:49 +1100

    add ToDos

--- test/polymer-markdown-generator-test.js
+++ test/polymer-markdown-generator-test.js
@@ -3,5 +3,8 @@ var generator = require('../polymer-markdown-generator.js');
 
 describe('Polymer Markdown Generator', function() {
   // ToDo: add a kind of test that test the input and output of the CLI application
+  // 1. correct file is save
+  // 2. flags are processed correctly
+  // 3. errors when no file is passed
   it('should have a methodFormatter Method'); 
 });
