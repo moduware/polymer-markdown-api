6d00f9844b5b20332caff02f22068a7a907a53d9
Clint <clintp1@gmail.com>
2017-12-13 16:00:14 +1100

    add test when no file is passed
    correct error string will be log

--- test/polymer-markdown-generator-test.js
+++ test/polymer-markdown-generator-test.js
@@ -1,10 +1,27 @@
 const expect = require('chai').expect;
-var generator = require('../polymer-markdown-generator.js');
+const generator = require('../polymer-markdown-generator.js');
+const exec = require('child_process').exec;
+
+let outputString;
+let errorWhenNoFilePass = "Single file to process must be specified";
+
+
+// console.log('generator', generator);
 
 describe('Polymer Markdown Generator', function() {
+  before(function(done) {
+    const child = exec('node polymer-markdown-generator.js', function (err, stdout, stderr) {
+      console.log('stdout', stdout);
+      outputString = stdout = stdout.replace(/(\r\n|\n|\r)/gm, "");;
+      done();
+    });
+  });
+
   // ToDo: add a kind of test that test the input and output of the CLI application
-  // 1. correct file is save
-  // 2. flags are processed correctly
-  // 3. errors when no file is passed
-  it('should have a methodFormatter Method'); 
+  it('should have error when no file is passed to parse', function () {
+    expect(outputString).to.equal(errorWhenNoFilePass);
+  });
+  
+  it('should have a correct file is save');
+  it('should processed --flags correctly');
 });
