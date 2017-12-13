8070f56a78d9a9a1cfbe7eb3e561e7c89f520bd1
Clint <clintp1@gmail.com>
2017-12-13 14:23:48 +1100

    add test property-formatter-test.js
    add test with input string returns correct format string for markdown
    file

--- test/property-formatter-test.js
+++ test/property-formatter-test.js
@@ -3,5 +3,23 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('propertyFormatter', function () {
-  it('formats property inside element into markdown');
+  let propertyMap = new Map();
+
+  propertyMap.set('big', { 
+    name: 'big',
+    type: 'boolean',
+    description: 'Common for iOS and Android Platform'
+  });
+
+  let morphButton = {
+    properties: propertyMap
+  };
+  console.log('propertyMap', propertyFormatter(morphButton));
+  
+  it('formats property inside element into markdown', function() {
+    let markdownOutput = propertyFormatter(morphButton);
+
+    expect(markdownOutput).to.include('**big**: _boolean_');
+    expect(markdownOutput).to.include('Common for iOS and Android Platform');
+  });
 });
