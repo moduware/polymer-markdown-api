2e101f1c045993759dd7135f3fbecce6074f76d6
Clint <clintp1@gmail.com>
2017-12-13 13:45:16 +1100

    clean logs  js/method-formatter.js

--- js/method-formatter.js
+++ js/method-formatter.js
@@ -3,8 +3,6 @@ const createStringOfArgumentNames = require('./create-string-of-argument-names')
 function methodFormatter(element) {
   let markdown = '';
   markdown += '## Methods\n\n';
-  let methods = element[Object.getOwnPropertyNames(element)[3]];
-  console.log('element', methods.entries());
   
   for (const [name, method] of element.methods) {
     let argumentNames = createStringOfArgumentNames(method.params);
