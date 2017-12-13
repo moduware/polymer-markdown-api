ebd434882d742c37c18db97f036707ea2268f9c8
Clint <clintp1@gmail.com>
2017-12-13 13:34:51 +1100

    add method-formatter-test.js
    add test to check given some string input will output string in certain
    markdown format

---
+++ test/method-formatter-test.js
@@ -0,0 +1,40 @@
+const methodFormatter = require('../js/method-formatter');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('methodFormatter', function() {
+  let methodsMap = new Map();
+  // methodsMap.set('first', []);
+  methodsMap.set('colorAssigned', 
+    {
+      name: 'colorAssigned',
+      type: 'Function',
+      description: 'function will fire a console.warn message',
+      params: [{
+        name: 'oldValue',
+        type: '[any]',
+        defaultValue: undefined,
+        rest: undefined,
+        description: 'old value of color property'
+      },
+      {
+        name: 'newValue',
+        type: 'any',
+        defaultValue: undefined,
+        rest: undefined,
+        description: 'new value of color property'
+      }]
+    });
+
+  let morphButton = {
+    methods: methodsMap,
+  };
+  
+  it('formats methods inside element', function() {
+    let result = methodFormatter(morphButton)
+    console.log(result);
+    expect(result).to.include('## Methods');
+    expect(result).to.include('**colorAssigned(oldValue, newValue)**');
+    expect(result).to.include('function will fire a console.warn message');
+  });
+});
