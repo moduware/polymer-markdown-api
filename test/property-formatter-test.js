b62dcb60bbc650c020ad9cd04d6b01f9b6332ad5
Clint <clintp1@gmail.com>
2017-12-13 13:52:05 +1100

    add test file for property formatter method

---
+++ test/property-formatter-test.js
@@ -0,0 +1,7 @@
+const propertyFormatter = require('../js/property-formatter');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('propertyFormatter', function () {
+  it('formats property inside element into markdown');
+});
