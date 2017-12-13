4a8f52b8598a8efd033a90a41ad943cff61ff009
Clint <clintp1@gmail.com>
2017-12-13 13:49:13 +1100

    remove irrelavant test
    ToDo: add test for the CLI app input / output is correct
    1. correct file is save
    2. flags are processed correctly
    3. errors when no file is passed

--- test/polymer-markdown-generator-test.js
+++ test/polymer-markdown-generator-test.js
@@ -2,32 +2,6 @@ const expect = require('chai').expect;
 var generator = require('../polymer-markdown-generator.js');
 
 describe('Polymer Markdown Generator', function() {
-  context('methodFormatter', function() {
-    it('test for method formatter output', function() {
-      var arr = [];
-      
-      expect(arr.length).to.equal(0);
-    });
-
-    it('should have a methodFormatter Method', function() {
-      expect(typeof generator).to.be.equal('object');
-      expect(typeof generator._methodFormatter).to.be.equal('function');
-    });
-  });
-  
-  context('propertyFormatter', function() {
-    it('should have a propertyFormatter Method', function() {
-      expect(typeof generator).to.be.equal('object');
-      expect(typeof generator._propertyFormatter).to.be.equal('function');
-    });
-  });
-
-  context('createStringOfArguments', function(){
-    it('should have a createStringOfArgumentNames Method', function () {
-      expect(typeof generator).to.be.equal('object');
-      expect(typeof generator._createStringOfArgumentNames).to.be.equal('function');
-    });
-
-    it('takes an array and returns comma seperated string of names');
-  });
+  // ToDo: add a kind of test that test the input and output of the CLI application
+  it('should have a methodFormatter Method'); 
 });
